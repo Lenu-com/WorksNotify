641c5af6dcd3446f19bc7c89bf3024263b4cf150
Lenu-com <oretyan@icloud.com>
2024-01-25 00:59:32 +0900

    依存関係の改善に伴い修正

--- tests/test_notify.py
+++ tests/test_notify.py
@@ -1,17 +1,19 @@
 from unittest.mock import patch
 from app.infrastructure.notify import Notify
+from app.domain.objects.Message import Message
 
 
 @patch('requests.post')
-def test_send_line_notify(mock_post):
+def test_send(mock_post) -> None:
     test_message = "Test message"
     test_token = "test_token"
     
     notify = Notify(line_notify_token=test_token)
-    notify.send_line_notify(test_message)
+    notify.send_message(Message(test_message))
 
     mock_post.assert_called_once_with(
         'https://notify-api.line.me/api/notify',
         headers={'Authorization': f'Bearer ' + test_token},
         data={'message': test_message}
     )
+
