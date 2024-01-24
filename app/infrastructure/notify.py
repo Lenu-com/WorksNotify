4611d34def42014d83d776f9b521594f3992a36f
Lenu-com <oretyan@icloud.com>
2024-01-24 23:43:10 +0900

    Notifyクラスの引数にtokenを指定できるよう修正

--- app/infrastructure/notify.py
+++ app/infrastructure/notify.py
@@ -4,14 +4,15 @@ from typing import Final
 
 
 class Notify:
-    line_notify_token: Final[str] = os.getenv('LINE_NOTIFY_TOKEN')
-    line_notify_api: Final[str] = 'https://notify-api.line.me/api/notify'
+    _line_notify_api: Final[str] = 'https://notify-api.line.me/api/notify'
     
-    def __init__(self):
-        if self.line_notify_token is None:
+    def __init__(self, line_notify_token=os.getenv('LINE_NOTIFY_TOKEN')):
+        if line_notify_token is None:
             raise Exception('LINE_NOTIFY_TOKEN is not set.')
+        self._line_notify_token = line_notify_token
+        
     
     def send_line_notify(self, notifycation_message):
-        headers = {'Authorization': f'Bearer {self.line_notify_token}'}
+        headers = {'Authorization': f'Bearer {self._line_notify_token}'}
         data = {'message': notifycation_message}
-        requests.post(self.line_notify_api, headers = headers, data = data)
+        requests.post(self._line_notify_api, headers = headers, data = data)
--- tests/test_notify.py
+++ tests/test_notify.py
@@ -7,8 +7,7 @@ def test_send_line_notify(mock_post):
     test_message = "Test message"
     test_token = "test_token"
     
-    notify = Notify()
-    notify.line_notify_token = test_token
+    notify = Notify(line_notify_token=test_token)
     notify.send_line_notify(test_message)
 
     mock_post.assert_called_once_with(
