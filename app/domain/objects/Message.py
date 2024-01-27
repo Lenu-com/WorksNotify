4878fb2a3abbcb86bc650eb469c24d6969ca1cae
Lenu-com <oretyan@icloud.com>
2024-01-27 13:12:52 +0900

    initの例外処理をExceptionからValueErrorに変更

--- app/domain/objects/Message.py
+++ app/domain/objects/Message.py
@@ -1,9 +1,9 @@
 class Message:
     def __init__(self, message_text: str) -> None:
         if message_text is None:
-            raise Exception('message_text is None.')
+            raise ValueError('message_text is None.')
         if len(message_text) < 1:
-            raise Exception('message_text is empty.')
+            raise ValueError('message_text is empty.')
         self._text = message_text
         
     
