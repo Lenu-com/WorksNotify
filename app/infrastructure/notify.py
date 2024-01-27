b7a762f4eef29d17b5a218f066617310626d713b
Lenu-com <oretyan@icloud.com>
2024-01-28 01:08:02 +0900

    各インスタンス用の例外に差し替え

--- app/infrastructure/notify.py
+++ app/infrastructure/notify.py
@@ -1,19 +1,20 @@
 from typing import Final
+from app.domain.objects.message import Message
 from app.infrastructure.interfaces.i_http_client import IHttpClient
 from app.domain.services.interfaces.i_message_service import IMessageService
-from app.domain.objects.message import Message
+from app.infrastructure.exceptions.notify_exceptions import HttpClientNoneException, MessageServiceNoneException, LineNotifyTokenNoneException
 
 
 class Notify:
     _line_notify_api: Final[str] = 'https://notify-api.line.me/api/notify'
     
-    def __init__(self, http_client: IHttpClient, message_service: IMessageService, line_notify_token:str) -> None:
+    def __init__(self, http_client: IHttpClient, message_service: IMessageService, line_notify_token: str) -> None:
         if http_client is None:
-            raise Exception('http_client is None.')
+            raise HttpClientNoneException()
         if message_service is None:
-            raise Exception('message_service is None.')
+            raise MessageServiceNoneException()
         if line_notify_token is None:
-            raise Exception('LINE_NOTIFY_TOKEN is not set.')
+            raise LineNotifyTokenNoneException()
         self._line_notify_token = line_notify_token
         self._http_client = http_client
         self._message_service = message_service
