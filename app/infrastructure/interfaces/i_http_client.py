a0d9fae3f7468d60ecd220dab06c59b57d1d8a6a
Lenu-com <oretyan@icloud.com>
2024-01-28 01:08:52 +0900

    引数の型ヒントを追加

--- app/infrastructure/interfaces/i_http_client.py
+++ app/infrastructure/interfaces/i_http_client.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
+from typing import Dict
 
 
 class IHttpClient(ABC):
     @abstractmethod
-    def post(self, url, headers, data):
+    def post(self, url: str, headers: Dict[str, str], data: Dict[str, str]) -> None:
         raise NotImplementedError()
