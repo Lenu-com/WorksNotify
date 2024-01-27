7446724d2a503d917a4aa1258f466a973fd3f0ff
Lenu-com <oretyan@icloud.com>
2024-01-28 01:07:30 +0900

    引数に型ヒントを追加

--- app/domain/services/message_service.py
+++ app/domain/services/message_service.py
@@ -1,8 +1,9 @@
 from typing import Dict
 from app.domain.services.interfaces.i_message_service import IMessageService
+from app.domain.objects.message import Message
 
 
 class MessageService(IMessageService):
     @classmethod
-    def create_message_data(cls, message) -> Dict[str, str]:
+    def create_message_data(cls, message: Message) -> Dict[str, str]:
         return {'message': message.text}
